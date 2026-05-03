a46de56e70b06375861d926a0c75aa1d7b0cc449
Giuseppe Celentano <giuseppecelentano4@gmail.com>
2026-05-03 11:47:35 +0200

    "Added delete() and update() functions to models/canotta.php"

--- models/canotta.php
+++ models/canotta.php
@@ -85,13 +85,73 @@ class Canotta{
     
   }
 
-  function delete(){
+ function update() {
     
-  }
+    $query = "UPDATE " . $this->table_name . "
+              SET
+                giocatore = :giocatore,
+                squadra = :squadra,
+                numero = :numero,
+                tipo = :tipo,
+                anno = :anno,
+                prezzo_originale = :prezzo_originale,
+                percentuale_sconto = :percentuale_sconto
+              WHERE
+                id_canotta = :id";
 
-  function update(){
     
-  }
+    $stmt = $this->conn->prepare($query);
+
+    //Sanitizzazione dei dati 
+    $this->giocatore = htmlspecialchars(strip_tags($this->giocatore));
+    $this->squadra = htmlspecialchars(strip_tags($this->squadra));
+    $this->numero = htmlspecialchars(strip_tags($this->numero));
+    $this->tipo = htmlspecialchars(strip_tags($this->tipo));
+    $this->anno = htmlspecialchars(strip_tags($this->anno));
+    $this->prezzo_originale = htmlspecialchars(strip_tags($this->prezzo_originale));
+    $this->percentuale_sconto = htmlspecialchars(strip_tags($this->percentuale_sconto));
+    $this->id_canotta = htmlspecialchars(strip_tags($this->id_canotta));
+
+    //Binding dei parametri
+    $stmt->bindParam(":giocatore", $this->giocatore);
+    $stmt->bindParam(":squadra", $this->squadra);
+    $stmt->bindParam(":numero", $this->numero);
+    $stmt->bindParam(":tipo", $this->tipo);
+    $stmt->bindParam(":anno", $this->anno);
+    $stmt->bindParam(":prezzo_originale", $this->prezzo_originale);
+    $stmt->bindParam(":percentuale_sconto", $this->percentuale_sconto);
+    $stmt->bindParam(":id", $this->id_canotta);
+
+    
+    if ($stmt->execute()) {
+        return true;
+    }
+    return false;
+}
+
+  function delete() {
+    
+    $query = "DELETE FROM " 
+            . $this->table_name . 
+            " WHERE id_canotta = :id";
+
+   
+    $stmt = $this->conn->prepare($query);
+
+    // 3. Sanitizzazione
+    $this->id_canotta = htmlspecialchars(strip_tags($this->id_canotta));
+
+    //Binding dell' ID
+    $stmt->bindParam(":id", $this->id_canotta);
+
+   
+    if ($stmt->execute()) {
+        return true;
+    }
+    return false;
+}
+
+ 
 
 }
 
